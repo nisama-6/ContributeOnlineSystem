f786d4a56a7dc0a19d599f7dbeb36c4af9bd2b0d
niran666 <1291614525@qq.com>
2019-12-24 15:09:32 +0800

    add

---
+++ src/main/java/com/contribute/demo/repository/UsermessageRepository.java
@@ -0,0 +1,16 @@
+package com.contribute.demo.repository;
+
+import com.contribute.demo.pojo.Account;
+import com.contribute.demo.pojo.Usermessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @ClassName : UsermessageRepository
+ * @Description : TODO
+ * @Author : niran
+ * @Date : 2019/12/24
+ **/
+
+public interface UsermessageRepository extends JpaRepository<Usermessage, Long> {
+
+}
