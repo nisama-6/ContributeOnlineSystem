a1309f01b282ccba3f383c0b65bfdbc4ef88e674
niran666 <1291614525@qq.com>
2019-12-24 14:08:28 +0800

    add

--- src/main/java/com/contribute/demo/pojo/Account.java
+++ src/main/java/com/contribute/demo/pojo/Account.java
@@ -1,5 +1,9 @@
 package com.contribute.demo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
 /**
  * @ClassName : Account
  * @Description : TODO
@@ -7,8 +11,24 @@ package com.contribute.demo.pojo;
  * @Date : 2019/12/20
  **/
 
+@Entity
+@Table(name="tb_account")
 public class Account {
+    @Id
+    @JsonIgnore
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private String level;
+
+    private String exp;
+
+    private String nickname;
+
     private String identity;
 
     public String getIdentity() {
@@ -25,5 +45,45 @@ public class Account {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getExp() {
+        return exp;
+    }
+
+    public void setExp(String exp) {
+        this.exp = exp;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
     }
 }
