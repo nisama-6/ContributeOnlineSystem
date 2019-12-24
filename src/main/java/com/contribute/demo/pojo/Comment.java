7c33b22369efc0a1a76368028c64c2f3394daa79
ntdr2009 <ntdr@qq.com>
2019-12-24 14:34:46 +0800

    建立了pojo

--- src/main/java/com/contribute/demo/pojo/Comment.java
+++ src/main/java/com/contribute/demo/pojo/Comment.java
@@ -38,6 +38,7 @@ public class Comment {
                 ", suggestion='" + suggestion + '\'' +
                 ", suggestdate='" + suggestdate + '\'' +
                 '}';
+
     }
 
     public int getId() {
